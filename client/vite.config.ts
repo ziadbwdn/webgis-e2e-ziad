ca831f9749a72a9d84f95a00b33ebaad10fb64b8
ziadbwdn <user@LAPTOP-SDH7GN63.localdomain>
2025-11-21 20:49:31 +0700

    reconfiguration of vite config code file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
@@ -6,7 +7,14 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
-    minify: false
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        home: resolve(__dirname, 'home.html')
+      }
+    }
   },
   server: {
     host: '0.0.0.0',
